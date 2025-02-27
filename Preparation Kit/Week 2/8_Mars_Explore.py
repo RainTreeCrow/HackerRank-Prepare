576793a7f4947347ff061dbb2773e02791b8b6af
RainTreeCrow <1683762615@qq.com>
2025-02-27 13:58:30 +0000

    mars explore

---
+++ Preparation Kit/Week 2/8_Mars_Explore.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    count = 0
+    target = 'SOS'
+    for i, c in enumerate(s):
+        if c != target[i % 3]:
+            count += 1
+    
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
